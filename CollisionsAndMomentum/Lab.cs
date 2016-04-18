93467656aa3493d3ffd85601968d743ac71f629e
Andrew Seba <sebaturtle@gmail.com>
2016-04-18 16:21:59 -0500

    Add project files.

---
+++ CollisionsAndMomentum/Lab.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using VectorClassLab;
+
+namespace CollisionsAndMomentum
+{
+    /// <summary>
+    /// @Author: Andrew Seba
+    /// @Description: Models a reflection off of a plane.   
+    /// </summary>
+    class Lab
+    {
+
+        /*
+         * Simple Test Cases:
+         * M = mass
+         * V = velocity
+         * 
+         * Collisions:                             * Reflections:                   
+         *  M1 = 10                                *<Vi> = <10,10, -10>         
+         *  M2 = 10                                *Plane defined by            
+         *  e = 1                                  *<1, 0, 0>, <0, 1, 0>        
+         *  V1i = 10                               *                            
+         *  V2i = 0                                *e = 1, <Vf> = <10, 10, 10>  
+         *                                         *e = 0, <Vf> = <10, 10, 0>   
+         *  V1f = 0, V2f = 10   V1f = V2f = 5      *                            
+         */
+
+        public void DoReflection()
+        {
+            Console.WriteLine("Collision and Momentum");
+
+            Console.Write("What is the initial velocity vector for the object (Rectangular Coordinates)?");
+
+            Vector3D velocity = GetVector("Velocity Vector");
+
+            Console.WriteLine("What is the coefficient of restitution?");
+            float coeff = (float)Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Plane definitionVectors: ");
+            Vector3D p1 = GetVector("Vector1 of the Plane");
+            Vector3D p2 = GetVector("Vector2 of the Plane");
+
+            Vector3D N = p1.Cross(p2);
+
+            Vector3D reflection = velocity - (N & (N * velocity) & (1 + coeff));
+
+            reflection.PrintRect();
+
+            Console.ReadLine();
+        }
+
+        public void DoCollision()
+        {
+            Console.WriteLine("1D Collision");
+            Console.Write("Velocity 1: ");
+            float velocity1i = (float)Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Velocity 2: ");
+            float velocity2i = (float)Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Mass 1: ");
+            float mass1 = (float)Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Mass 2: ");
+            float mass2 = (float)Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Coefficient of Restitution: ");
+            float coeff = (float)Convert.ToDouble(Console.ReadLine());
+
+
+            float velocity1f = ((mass1 - coeff * mass2) * velocity1i + (1 + coeff) * mass2 * velocity2i) / (mass1 + mass2);
+            float velocity2f = velocity1f + (coeff * (velocity1i - velocity2i));
+            Console.WriteLine("V1f: " + velocity1f.ToString("F2"));
+            Console.WriteLine("V2f: " + velocity2f.ToString("F2"));
+
+            Console.ReadKey();
+
+        }
+
+        #region Getters
+        /// <summary>
+        /// Gets a vector from the user in x,y,z coordinates.
+        /// </summary>
+        /// <returns>New Vector</returns>
+        private Vector3D GetVector()
+        {
+            Console.Write("\nx: ");
+            float x = (float)Convert.ToDouble(Console.ReadLine());
+            Console.Write("y: ");
+            float y = (float)Convert.ToDouble(Console.ReadLine());
+            Console.Write("z: ");
+            float z = (float)Convert.ToDouble(Console.ReadLine());
+
+            return new Vector3D(x, y, z);
+        }
+
+
+        /// <summary>
+        /// Gets a vector from the user in x,y,z coordinates. with a message
+        /// parameter to tell the user what the vector is for.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        private Vector3D GetVector(string message)
+        {
+            Console.WriteLine(message);
+            Console.Write("x: ");
+            float x = (float)Convert.ToDouble(Console.ReadLine());
+            Console.Write("y: ");
+            float y = (float)Convert.ToDouble(Console.ReadLine());
+            Console.Write("z: ");
+            float z = (float)Convert.ToDouble(Console.ReadLine());
+
+            return new Vector3D(x, y, z);
+        }
+
+        #endregion
+    }
+}
---
+++ CollisionsAndMomentum/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollisionsAndMomentum
+{
+    /// <summary>
+    /// @Author: Andrew Seba
+    /// @Description:
+    /// Modeling the reflection of an object off a plane surface.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lab lab = new Lab();
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("1: Reflection");
+                Console.WriteLine("2: Collision");
+
+                switch ((int)Convert.ToInt16(Console.ReadLine()))
+                {
+                    case 1:
+                        Console.Clear();
+                        lab.DoReflection();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        lab.DoCollision();
+                        break;
+                }
+            } while (true);
+            
+
+
+
+        }
+    }
+}
